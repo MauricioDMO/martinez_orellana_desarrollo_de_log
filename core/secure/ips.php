3e60dd2d9f0e6e4d3f130e1f34d9cba2b092f881
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2025-05-07 19:58:45 -0600

    Add IP range configuration file

---
+++ core/secure/ips.php
@@ -0,0 +1,8 @@
+<?php
+$rango = array(
+  "127.0.0.1/32", // Permite localhost
+  "192.168.1.0/24", // Permite todos los hosts de la subred
+  "10.10.10.*", // Permite todos los hosts de la subred
+  "172.16.10.1-172.16.10.50" // Permite un rango de IPs especifico
+);
+?>
