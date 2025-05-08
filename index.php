cffb94a1590277f5b18ca2f2415041f5e8baacf3
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2025-05-07 19:59:33 -0600

    Starting log logic

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+include("core/inc/funciones.inc.php");
+include("core/secure/ips.php");
+
+$archivo = "./logs/log.log";
+$ip = ip_in_ranges($_SERVER['REMOTE_ADDR'], $rango);
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 
